b3da158b1c45588ee0d6249a80a7066cf93a0567
anifecelil@gmail.com <anifecelil@gmail.com>
2020-04-04 22:16:33 +0300

    Initial code from the template added

---
+++ Web/JewelryShop.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,47 @@
+﻿namespace JewelryShop.Web.Areas.Identity.Pages.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using JewelryShop.Data.Models;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    [AllowAnonymous]
+#pragma warning disable SA1649 // File name should match first type name
+    public class LogoutModel : PageModel
+#pragma warning restore SA1649 // File name should match first type name
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<LogoutModel> logger;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await this.signInManager.SignOutAsync();
+            this.logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return this.LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return this.RedirectToPage();
+            }
+        }
+    }
+}
