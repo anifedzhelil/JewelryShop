1d0525e542c12b3059a36adb30e8ca6a9f475de0
anifecelil@gmail.com <anifecelil@gmail.com>
2020-04-29 16:46:13 +0300

    Add  register page

--- Web/JewelryShop.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Web/JewelryShop.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -6,6 +6,7 @@
     using System.Linq;
     using System.Text.Encodings.Web;
     using System.Threading.Tasks;
+
     using JewelryShop.Common;
     using JewelryShop.Data.Models;
     using JewelryShop.Services.Data;
--- Web/JewelryShop.Web/Controllers/OrdersController.cs
+++ Web/JewelryShop.Web/Controllers/OrdersController.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+
     using JewelryShop.Common;
     using JewelryShop.Data.Models;
     using JewelryShop.Services.Data;
